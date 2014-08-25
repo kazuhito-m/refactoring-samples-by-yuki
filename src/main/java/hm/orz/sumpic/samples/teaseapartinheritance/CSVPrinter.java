b619413bd03b3a557e6a5383203ca87f97bedd11
kazuhito-m <kazuhito.sumpic@gmail.com>
2014-08-26 08:16:43 +0900

    リファクタリング。本格的に委譲用の抽象メソッドを追加。広域変数も追加。

--- src/main/java/hm/orz/sumpic/samples/teaseapartinheritance/CSVPrinter.java
+++ src/main/java/hm/orz/sumpic/samples/teaseapartinheritance/CSVPrinter.java
@@ -4,6 +4,14 @@ import java.io.IOException;
 
 public abstract class CSVPrinter {
 
+	protected final CSVReader _csvReader;
+	
+	protected CSVPrinter(CSVReader reader) {
+		_csvReader = reader;
+	}
+	
     public abstract void print() throws IOException;
 
+    public abstract String[] readCSV() throws IOException;
+    
 }
