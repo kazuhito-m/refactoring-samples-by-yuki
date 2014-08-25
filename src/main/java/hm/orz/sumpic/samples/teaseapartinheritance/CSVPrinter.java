7935c282e78fb41c1844c71cf3cba7d78f5e860d
kazuhito-m <kazuhito.sumpic@gmail.com>
2014-08-26 08:08:46 +0900

    リファクタリング。「CSVを表示する」という責務を持つ、メソッド委譲用のクラスを定義する。一つメソッドを予め抽象メソッドとして定義しておく。

---
+++ src/main/java/hm/orz/sumpic/samples/teaseapartinheritance/CSVPrinter.java
@@ -0,0 +1,9 @@
+package hm.orz.sumpic.samples.teaseapartinheritance;
+
+import java.io.IOException;
+
+public abstract class CSVPrinter {
+
+    public abstract void print() throws IOException;
+
+}
