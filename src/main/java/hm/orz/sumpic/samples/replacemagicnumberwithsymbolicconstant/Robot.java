cba9a0e0615efbdbc4c0ee5b2d5f484bca6143ec
k-miura-biz <kazuhito.sumpic@gmail.com>
2014-09-16 18:51:47 +0900

    総合してリファクタリングじゃい!

--- src/main/java/hm/orz/sumpic/samples/replacemagicnumberwithsymbolicconstant/Robot.java
+++ src/main/java/hm/orz/sumpic/samples/replacemagicnumberwithsymbolicconstant/Robot.java
@@ -6,23 +6,23 @@ public class Robot {
 //    public static final int COMMAND_STOP = 1;
 //    public static final int COMMAND_JUMP = 2;
 
-//    public enum Command {
-//        WALK,
-//        STOP,
-//        JUMP,
-//    };
+    public enum Command {
+        WALK,
+        STOP,
+        JUMP,
+    };
 
 
     private final String _name;
     public Robot(String name) {
         _name = name;
     }
-    public void order(int command) {
-        if (command == 0) {
+    public void order(Command command) {
+        if (command == Command.WALK) {
             System.out.println(_name + " walks.");
-        } else if (command == 1) {
+        } else if (command == Command.STOP) {
             System.out.println(_name + " stops.");
-        } else if (command == 2) {
+        } else if (command == Command.JUMP) {
             System.out.println(_name + " jumps.");
         } else {
             System.out.println("Command error. command = " + command);
