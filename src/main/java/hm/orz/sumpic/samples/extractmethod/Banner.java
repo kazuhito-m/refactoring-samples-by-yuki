f823dcc952ea7a591ba33fdff713b584d35f1995
kazuhito_m <kazuhito.sumpic@gmail.com>
2015-07-13 23:58:47 +0900

    サンプル追加。

---
+++ src/main/java/hm/orz/sumpic/samples/extractmethod/Banner.java
@@ -0,0 +1,28 @@
+package hm.orz.sumpic.samples.extractmethod;
+
+public class Banner {
+    private final String _content;
+    public Banner(String content) {
+        _content = content;
+    }
+    public void print(int times) {
+        // Prints border.
+        System.out.print("+");
+        for (int i = 0; i < _content.length(); i++) {
+            System.out.print("-");
+        }
+        System.out.println("+");
+
+        // Prints content.
+        for (int i = 0; i < times; i++) {
+            System.out.println("|" + _content + "|");
+        }
+
+        // Prints border.
+        System.out.print("+");
+        for (int i = 0; i < _content.length(); i++) {
+            System.out.print("-");
+        }
+        System.out.println("+");
+    }
+}
