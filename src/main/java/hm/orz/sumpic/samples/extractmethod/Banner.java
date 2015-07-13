e93c984c5f916b3151e0b9ee0672c36379a5a18d
kazuhito_m <kazuhito.sumpic@gmail.com>
2015-07-14 00:22:36 +0900

    うまく行ったので、全てをメソッド化。

--- src/main/java/hm/orz/sumpic/samples/extractmethod/Banner.java
+++ src/main/java/hm/orz/sumpic/samples/extractmethod/Banner.java
@@ -3,21 +3,26 @@ package hm.orz.sumpic.samples.extractmethod;
 public class Banner {
 
     private final String _content;
-    
+
     public Banner(String content) {
         _content = content;
     }
 
     public void print(int times) {
         printBorder();
+        printContent(times);
+        printBorder();
+    }
 
-        // Prints content.
+    /**
+     * Prints content.
+     *
+     * @param times 繰り返す数。
+     */
+    private void printContent(int times) {
         for (int i = 0; i < times; i++) {
             System.out.println("|" + _content + "|");
         }
-
-        // Prints border.
-        printBorder();
     }
 
     /**
