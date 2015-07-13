57c7d17c7547434a284ec1fc091eed0063a970c9
kazuhito_m <kazuhito.sumpic@gmail.com>
2015-07-14 00:20:02 +0900

    リファクタリング。メソッド抽出。

--- src/main/java/hm/orz/sumpic/samples/extractmethod/Banner.java
+++ src/main/java/hm/orz/sumpic/samples/extractmethod/Banner.java
@@ -1,18 +1,15 @@
 package hm.orz.sumpic.samples.extractmethod;
 
 public class Banner {
+
     private final String _content;
+    
     public Banner(String content) {
         _content = content;
     }
 
     public void print(int times) {
-        // Prints border.
-        System.out.print("+");
-        for (int i = 0; i < _content.length(); i++) {
-            System.out.print("-");
-        }
-        System.out.println("+");
+        printBorder();
 
         // Prints content.
         for (int i = 0; i < times; i++) {
@@ -20,6 +17,13 @@ public class Banner {
         }
 
         // Prints border.
+        printBorder();
+    }
+
+    /**
+     * Prints border.
+     */
+    private void printBorder() {
         System.out.print("+");
         for (int i = 0; i < _content.length(); i++) {
             System.out.print("-");
