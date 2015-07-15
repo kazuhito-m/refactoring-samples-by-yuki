42575d04c39340d85513a2096dd26f2e9a917d98
kazuhito-m <kazuhito.sumpic@gmail.com>
2015-07-16 00:07:39 +0900

    リファクタリング。不要になったSetterを除去。

--- src/main/java/hm/orz/sumpic/samples/extractclass/Author.java
+++ src/main/java/hm/orz/sumpic/samples/extractclass/Author.java
@@ -8,8 +8,7 @@ public class Author {
     private String _name;
     private String _mail;
 
-
-    public Author(String name , String mail) {
+    public Author(String name, String mail) {
         _name = name;
         _mail = mail;
     }
@@ -17,15 +16,8 @@ public class Author {
     public String getName() {
         return _name;
     }
+
     public String getMail() {
         return _mail;
-    }
-
-    public void setName(String name) {
-        _name = name;
-    }
-
-    public void setMail(String mail) {
-        _mail = mail;
     }
 }
