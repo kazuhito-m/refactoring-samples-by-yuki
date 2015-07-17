094ce610f7690c1a5419abd9a76deb35d7874038
kazuhito-m <kazuhito.sumpic@gmail.com>
2015-07-17 23:51:39 +0900

    10章のサンプルプログラムを追加。

---
+++ src/main/java/hm/orz/sumpic/samples/replaceerrorcodewithexception/Direction.java
@@ -0,0 +1,14 @@
+package hm.orz.sumpic.samples.replaceerrorcodewithexception;
+
+public class Direction {
+    public int _x;
+    public int _y;
+    public Direction(int x, int y) {
+        _x = x;
+        _y = y;
+    }
+    public void setDirection(int x, int y) {
+        _x = x;
+        _y = y;
+    }
+}
