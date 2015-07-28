2d9e289f42b779b91bc8737297becbb5664dbd67
kazuhito-m <kazuhito.sumpic@gmail.com>
2015-07-29 08:15:41 +0900

    リファクタリング。必要でなくなった定数・変数を削除。

--- src/main/java/hm/orz/sumpic/samples/replaceconstructorwithfactorymethod/Shape.java
+++ src/main/java/hm/orz/sumpic/samples/replaceconstructorwithfactorymethod/Shape.java
@@ -1,39 +1,17 @@
 package hm.orz.sumpic.samples.replaceconstructorwithfactorymethod;
 
-public class Shape {
-    public static final int TYPECODE_LINE = 0;
-    public static final int TYPECODE_RECTANGLE = 1;
-    public static final int TYPECODE_OVAL = 2;
+public abstract class Shape {
 
-    private final int _typecode;
     private final int _startx;
     private final int _starty;
     private final int _endx;
     private final int _endy;
 
-    private Shape(int typecode, int startx, int starty, int endx, int endy) {
-        _typecode = typecode;
+    Shape(int startx, int starty, int endx, int endy) {
         _startx = startx;
         _endx = endx;
         _starty = starty;
         _endy = endy;
-    }
-
-    public int getTypecode() {
-        return _typecode;
-    }
-
-    public String getName() {
-        switch (_typecode) {
-            case TYPECODE_LINE:
-                return "LINE";
-            case TYPECODE_RECTANGLE:
-                return "RECTANGLE";
-            case TYPECODE_OVAL:
-                return "OVAL";
-            default:
-                return null;
-        }
     }
 
     public String toString() {
@@ -43,41 +21,38 @@ public class Shape {
                 + "(" + _endx + ", " + _endy + ") ]";
     }
 
-    public void draw() {
-        switch (_typecode) {
-            case TYPECODE_LINE:
-                drawLine();
-                break;
-            case TYPECODE_RECTANGLE:
-                drawRectangle();
-                break;
-            case TYPECODE_OVAL:
-                drawOval();
-                break;
-            default:
-                ;
-        }
-    }
+    /**
+     * 図形を描画する。
+     */
+    public abstract void draw();
 
-    private void drawLine() {
-        System.out.println("drawLine: " + this.toString());
-        // ...
-    }
+    /**
+     * 自身の名前を返す。
+     * @return 名前。
+     */
+    public abstract String getName();
 
-    private void drawRectangle() {
-        System.out.println("drawRectangle: " + this.toString());
-        // ...
-    }
-
-    private void drawOval() {
-        System.out.println("drawOval: " + this.toString());
-        // ...
+    /**
+     * Lineのオブジェクトを作るファクトリメソッド。
+     */
+    public static Shape createLine(int startx, int starty, int endx, int endy) {
+        return new ShapeLine(startx,starty,endx,endy);
     }
 
     /**
-     * ファクトリメソッド。
+     * Ovalのオブジェクトを作るファクトリメソッド。
      */
-    public static Shape create(int typecode, int startx, int starty, int endx, int endy) {
-        return new Shape(typecode,startx,starty,endx,endy);
+    public static Shape createOval(int startx, int starty, int endx, int endy) {
+        return new ShapeOval(startx,starty,endx,endy);
     }
+
+
+    /**
+     * Rectangleのオブジェクトを作るファクトリメソッド。
+     */
+    public static Shape createRectangle(int startx, int starty, int endx, int endy) {
+        return new ShapeRectangle(startx,starty,endx,endy);
+    }
+
+
 }
