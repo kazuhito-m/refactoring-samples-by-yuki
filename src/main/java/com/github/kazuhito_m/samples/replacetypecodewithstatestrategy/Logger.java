d84e0ae562abbbed081e45f3f47064529662f08f
kazuhito-m <kazuhito.sumpic@gmail.com>
2016-10-23 04:20:40 +0900

    リファクタリング。enum側に振るまいを移動し、Loaggerは「ステータス管理と何を呼び出すか」だけを制御するよう変更。

--- src/main/java/com/github/kazuhito_m/samples/replacetypecodewithstatestrategy/Logger.java
+++ src/main/java/com/github/kazuhito_m/samples/replacetypecodewithstatestrategy/Logger.java
@@ -8,68 +8,21 @@ public class Logger {
     private State _state;
 
     public Logger() {
-        setState(STATE_STOPPED);
+        _state = STATE_STOPPED;
     }
 
     public void start() {
-        switch (getState()) {
-            case STATE_STOPPED:
-                output("** START LOGGING **");
-                setState(STATE_LOGGING);
-                break;
-            case STATE_LOGGING:
-            /* Do nothing */
-                break;
-            default:
-                output("Invalid state: " + getState());
-        }
+        _state.start();
+        _state = STATE_LOGGING;
     }
 
     public void stop() {
-        switch (getState()) {
-            case STATE_STOPPED:
-            /* Do nothing */
-                break;
-            case STATE_LOGGING:
-                output("** STOP LOGGING **");
-                setState(STATE_STOPPED);
-                break;
-            default:
-                output("Invalid state: " + getState());
-        }
+        _state.stop();
+        _state = STATE_STOPPED;
     }
 
     public void log(String info) {
-        switch (getState()) {
-            case STATE_STOPPED:
-                output("Ignoring: " + info);
-                break;
-            case STATE_LOGGING:
-                output("Logging: " + info);
-                break;
-            default:
-                output("Invalid state: " + getState());
-        }
-    }
-
-    /**
-     * ログを媒体いに出力する。
-     * ※標準出力以外に出したく成った時用に、とりあえずまとめとく。
-     *
-     * @param log メッセージ。
-     */
-    private void output(String log) {
-        System.out.println(log);
-    }
-
-    // Setter/Getter (自己用)
-
-    private State getState() {
-        return this._state;
-    }
-
-    private void setState(State state) {
-        this._state = state;
+        _state.log(info);
     }
 
 }
--- src/main/java/com/github/kazuhito_m/samples/replacetypecodewithstatestrategy/State.java
+++ src/main/java/com/github/kazuhito_m/samples/replacetypecodewithstatestrategy/State.java
@@ -4,6 +4,55 @@ package com.github.kazuhito_m.samples.replacetypecodewithstatestrategy;
  * Loggerの状態を表すenum。
  */
 public enum State {
-    STATE_STOPPED,
-    STATE_LOGGING;
+
+    STATE_STOPPED {
+        @Override
+        public void start() {
+            output("** START LOGGING **");
+        }
+
+        @Override
+        public void stop() {
+            /* Do nothing */
+        }
+
+        @Override
+        public void log(String info) {
+            output("Ignoring: " + info);
+        }
+    },
+    STATE_LOGGING {
+        @Override
+        public void start() {
+            /* Do nothing */
+        }
+
+        @Override
+        public void stop() {
+            output("** STOP LOGGING **");
+        }
+
+        @Override
+        public void log(String info) {
+            output("Logging: " + info);
+        }
+    };
+
+
+    /**
+     * ログを媒体いに出力する。
+     * ※標準出力以外に出したく成った時用に、とりあえずまとめとく。
+     *
+     * @param log メッセージ。
+     */
+    private static void output(String log) {
+        System.out.println(log);
+    }
+
+    public abstract void start();
+
+    public abstract void stop();
+
+    public abstract void log(String info);
+
 }
