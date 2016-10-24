1a06cebe0e9f7ac6dfe6c20436ba68804eaf2709
kazuhito-m <kazuhito.sumpic@gmail.com>
2016-10-25 01:34:06 +0900

    テストを最新化。

--- src/test/java/com/github/kazuhito_m/samples/duplicateobserveddata/MainTest.java
+++ src/test/java/com/github/kazuhito_m/samples/duplicateobserveddata/MainTest.java
@@ -3,18 +3,22 @@ package com.github.kazuhito_m.samples.duplicateobserveddata;
 import junit.framework.JUnit4TestAdapter;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 
 public class MainTest {
-    @Test public void testMain() {
-        IntegerDisplay display = new IntegerDisplay();
-        display.setValue(1);
-        int actual = display.getValue();
-        int expected = 1;
-        assertEquals(expected, actual);
-    }
 
     public static junit.framework.Test suite() {
         return new JUnit4TestAdapter(MainTest.class);
     }
+
+    @Test
+    public void testMain() {
+        IntegerDisplay display = new IntegerDisplay();
+        display.setValue(1);
+        int actual = display.getValue();
+        int expected = 1;
+        assertThat(expected, is(actual));
+    }
+
 }
