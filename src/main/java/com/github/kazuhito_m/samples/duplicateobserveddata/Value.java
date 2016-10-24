c967974c5dbb95a18b378ace4babdd106cebc895
kazuhito-m <kazuhito.sumpic@gmail.com>
2016-10-25 02:31:46 +0900

    クラス追加。Valueを表すクラスを追加

---
+++ src/main/java/com/github/kazuhito_m/samples/duplicateobserveddata/Value.java
@@ -0,0 +1,10 @@
+package com.github.kazuhito_m.samples.duplicateobserveddata;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Value {
+    int value = 0;
+}
