df07158652cfecda897b042b00464f9c61fc54b2
kazuhito-m <kazuhito.sumpic@gmail.com>
2016-10-27 23:50:09 +0900

    実装。クラスの「必要な構造(継承、抽象メソッドの実装など)」を作る。

--- src/main/java/com/github/kazuhito_m/samples/duplicateobserveddata/Graph.java
+++ src/main/java/com/github/kazuhito_m/samples/duplicateobserveddata/Graph.java
@@ -1,7 +1,17 @@
 package com.github.kazuhito_m.samples.duplicateobserveddata;
 
+import java.awt.*;
+
 /**
  * 「円グラフ」を表すGUIクラス。
  */
-public class Graph {
+public class Graph extends Canvas implements ValueListener {
+
+    private static final int RETANGLE = 0;
+    private static final int CIRCLE = 1;
+    
+    @Override
+    public void valueChanged(ValueChangeEvent e) {
+
+    }
 }
