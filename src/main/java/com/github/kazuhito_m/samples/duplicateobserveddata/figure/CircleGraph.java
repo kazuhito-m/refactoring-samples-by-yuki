c7498cd132378623bdab093fdf1d74cac4ca3665
kazuhito-m <kazuhito.sumpic@gmail.com>
2016-10-28 23:02:36 +0900

    実装。円と資格の図形を実装。

--- src/main/java/com/github/kazuhito_m/samples/duplicateobserveddata/figure/CircleGraph.java
+++ src/main/java/com/github/kazuhito_m/samples/duplicateobserveddata/figure/CircleGraph.java
@@ -1,7 +1,19 @@
 package com.github.kazuhito_m.samples.duplicateobserveddata.figure;
 
+import java.awt.*;
+
 /**
- * Created by kazuhito on 16/10/28.
+ * 「円形」図形クラス。
  */
 public class CircleGraph extends Graph {
+
+    @Override
+    public void paint(Graphics g) {
+        g.setColor(this.graphValue > 0 ? Color.BLUE : Color.RED);
+        Rectangle bounds = getBounds();
+        int startAngle = 90;
+        int endAngle = -(int) (this.graphValue * 10.0);
+        g.fillArc(0, 0, bounds.width, bounds.height, startAngle, endAngle);
+    }
+
 }
--- src/main/java/com/github/kazuhito_m/samples/duplicateobserveddata/figure/RectangleGraph.java
+++ src/main/java/com/github/kazuhito_m/samples/duplicateobserveddata/figure/RectangleGraph.java
@@ -1,7 +1,32 @@
 package com.github.kazuhito_m.samples.duplicateobserveddata.figure;
 
+import java.awt.*;
+
 /**
- * Created by kazuhito on 16/10/28.
+ * 「長方形」図形クラス。
  */
 public class RectangleGraph extends Graph {
+
+    @Override
+    public void paint(Graphics g) {
+        g.setColor(this.graphValue > 0 ? Color.BLUE : Color.RED);
+        Rectangle bounds = getBounds();
+
+        int sign;
+        int minusWidthOn = 0;
+        if (graphValue > 0) {
+            sign = +1;
+            minusWidthOn = 0;
+        } else {
+            sign = -1;
+            minusWidthOn = 1;
+        }
+        int x, y, w, h;
+        w = (int) (bounds.width / 2 * (sign * graphValue) / 100.0);
+        h = bounds.height / 2;
+        x = bounds.width / 2 - (w * minusWidthOn);
+        y = bounds.height / 4;
+        g.fillRect(x, y, w, h);
+    }
+
 }
