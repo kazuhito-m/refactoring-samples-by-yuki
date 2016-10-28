4b8b7a4e6244c7ddd76d4405034dcc1f7d616e7a
kazuhito-m <kazuhito.sumpic@gmail.com>
2016-10-28 22:34:12 +0900

    実装。Graphクラス追加。

---
+++ src/main/java/com/github/kazuhito_m/samples/duplicateobserveddata/CircleGraph.java
@@ -0,0 +1,7 @@
+package com.github.kazuhito_m.samples.duplicateobserveddata;
+
+/**
+ * Created by kazuhito on 16/10/28.
+ */
+public class CircleGraph extends Graph {
+}
--- src/main/java/com/github/kazuhito_m/samples/duplicateobserveddata/Graph.java
+++ src/main/java/com/github/kazuhito_m/samples/duplicateobserveddata/Graph.java
@@ -7,11 +7,31 @@ import java.awt.*;
  */
 public class Graph extends Canvas implements ValueListener {
 
-    private static final int RETANGLE = 0;
+    private static final int RECTANGLE = 0;
     private static final int CIRCLE = 1;
-    
+
+    protected int graphValue;
+
+    /**
+     * ファクトリメソッド。
+     */
+    public static Graph createGraph(int type, int width, int height) {
+        Graph graph = null;
+        if (type == RECTANGLE) {
+            graph = new RectangleGraph();
+        } else if (type == CIRCLE) {
+            graph = new CircleGraph();
+        } else {
+            throw new RuntimeException("引数がおかしいです。図形タイプを指定してください。");
+        }
+        graph.setSize(new Dimension(width, height));
+        return graph;
+    }
+
     @Override
     public void valueChanged(ValueChangeEvent e) {
-
+        graphValue = e.getSoruce().getValue();
+        repaint();
     }
+
 }
